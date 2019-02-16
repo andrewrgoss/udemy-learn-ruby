4661cd7ff6467190beb049c5b242be5e086ee62b
Andrew Goss <andrew.goss@interfolio.com>
2019-02-16 15:22:17 -0500

    initial commit

---
+++ 3-numbers_and_booleans/intro_to_numbers.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+p 5.class
+p 0.next
+p 100
+p -837.class # integer
+
+p 5.987.class # float
+p 2.71
+p -10.23.class
+
+p 99_999_999_999_999_999_999_999_999_999_999_999_999_999_999_999_999.class
