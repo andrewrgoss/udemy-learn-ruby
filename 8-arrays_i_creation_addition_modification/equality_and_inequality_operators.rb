9eba7d4d986e40ea7aff0c272e8f252c8ed9c826
Andrew Goss <andrew.goss@interfolio.com>
2019-03-30 09:56:00 -0400

    initial commit

---
+++ 8-arrays_i_creation_addition_modification/equality_and_inequality_operators.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+a = [1, 2, 3]
+b = [1, 2, 3, 4]
+c = [3, 2, 1]
+d = [1, 2, 3]
+p a == b # returns false as compared arrays have to be exactly the same if it
+# comes to length, order and content.
+p a == c # false as the order is different
+p a == d
+
+p a != b # true
+p a != c # true
+p a != d # false
+
+a = %w[John Joanna Anna Tom]
+b = %w[John Joanna Anna tom]
+c = %w[John Joanna Anna Tom]
+p a == b
+p a == c
+p a != b
+p a != c
