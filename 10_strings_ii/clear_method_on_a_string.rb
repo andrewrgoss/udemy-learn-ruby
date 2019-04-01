7a3af503d402ca86d2505d78f305f09ed42aad2b
Andrew Goss <andrew.goss@interfolio.com>
2019-03-31 22:45:35 -0400

    initial commit

---
+++ 10_strings_ii/clear_method_on_a_string.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# This method clears the entire string content and makes it an empty string
+# After this method is called, the change of the string is permanent.
+word = 'Goodbye'
+p word
+word.clear
+p word
