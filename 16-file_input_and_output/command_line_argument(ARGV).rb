e586c83edc7851f6225b552eac7952e885ebe280
Andrew Goss <andrew.goss@interfolio.com>
2019-04-11 22:48:55 -0400

    initial commit

---
+++ 16-file_input_and_output/command_line_argument(ARGV).rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+ARGV.each do |argument|
+  number = argument.to_i # converting to_i as using ARGV we get a string
+  puts "The square of #{number} is #{number**2}"
+end
---
+++ 16-file_input_and_output/read_from_text_file_with_Ruby.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+File.open('novel.txt').each do
+  line # we will get an array here so we need
+  # to tell Ruby by the block how to read it; it doesn't work in the Atom editor
+  # needs to be open in the command prompt
+  puts line
+end
