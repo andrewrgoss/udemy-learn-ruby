c6363774d8fb4da18f368c1a49bbf556f8383df7
Andrew Goss <andrew.goss@interfolio.com>
2019-04-12 13:34:12 -0400

    initial commit

---
+++ 17-regular_expressions/exclude_characters.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+sales = 'I bought 9 apples, 25 bananas and 5 oranges at the store. '
+
+# if we want to remove everything from the string except consonants we need
+# to use  this special signs combination [^]
+puts sales.scan(/[^aeiouAEIOU, \s\d\.]/)
+puts
+puts sales.scan(/[^aeiouAEIOU, \s\d\.]/).length
+# // sign of regular expression
+# [^] excluding
+# \s white spaces
+# \d digits
+# \. period
