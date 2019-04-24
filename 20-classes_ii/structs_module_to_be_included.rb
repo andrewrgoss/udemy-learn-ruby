1f57b75150d41d575de2fcc916ac676c619889c7
Andrew Goss <andrew.goss@interfolio.com>
2019-04-24 17:55:34 -0400

    initial commit

---
+++ 20-classes_ii/structs_module_to_be_included.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# A struct represents a miniclass with less functonality than a typical Ruby class.
+# It is like a class with only instance variables. It is used when we only need
+# a framework for an object.
+
+module AppStore
+  App = Struct.new(:name, :developer, :version) # as argument we provide symbols
+  # representing instance variables on this struct
+
+  # defining a constant:
+  APPS = [App.new(:Chat, :facebook, 2.0),
+          App.new(:Twitter, :twitter, 5.8),
+          App.new(:Weather, :yahoo, 10.15)].freeze
+  def self.find_app(name)
+    APPS.find { |app| app.name == name }
+  end
+end
